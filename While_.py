8a480c077207d5ec13ff830d96225237bc10ee61
nf6813144 <nf6813144@gmail.com>
2024-10-21 08:43:11 -0700

    Create While_.py

---
+++ While_.py
@@ -0,0 +1,9 @@
+# memberikan nilai pada variabel z
+z= 25
+# kondisi perulangan while dimana nilai z lebih dari 10
+while z >= 10:   
+# mencetak nilai z      
+    print (z)
+# nilai z di kurangi dengan 2
+    z -= 2
+# perulangan akan berhenti jika nilai z lebih kecil atau sama dengan 10
